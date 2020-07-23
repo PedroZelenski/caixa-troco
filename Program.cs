d565fb199201518c70681f753f7a624c6257a176
Zelenski <zelenski.contato@gmail.com>
2020-07-23 14:37:06 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace caixa_troco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("INFORME O VALOR DO(s) PRODUTO(s): ");
+            double valor = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("INFORME A QUANTIA DE PAGAMENTO: ");
+            double pagamento = Convert.ToDouble(Console.ReadLine());
+
+            double troco = pagamento - valor;
+
+            if (troco > 0)
+                Console.WriteLine($"TROCO: {troco}");
+
+            if (troco == 0)
+                Console.WriteLine("NAO HA TROCO!");
+
+            if (troco < 0)
+                Console.WriteLine("A QUANTIA DE PAGAMENTO E INSUFICIENTE!");
+
+        }
+    }
+}
